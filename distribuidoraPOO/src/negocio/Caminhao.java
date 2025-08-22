13dcd8911adb4c5c87e4a5cb5d6a265d2119a4d5
arthur ramos <ramosarthuroliveira@gmail.com>
2025-08-22 16:13:49 -0300

    Criei a classe caminhao

--- distribuidoraPOO/src/negocio/Caminhao.java
+++ distribuidoraPOO/src/negocio/Caminhao.java
@@ -1,4 +1,50 @@
 package negocio;
 
 public class Caminhao {
+    private String placa;
+    private int capacidade;
+    private Motorista motorista;
+    private String status;
+
+    public Caminhao(String placa, String modelo, int capacidadeCarga, String status) {
+        this.placa = placa;
+        this.capacidade = capacidade;
+        this.status = status;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+
+    public Motorista getMotorista() {
+        return motorista;
+    }
+
+    public void setMotorista(Motorista motorista) {
+        this.motorista = motorista;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Caminhao{" +
+                "placa='" + placa + '\'' +
+                ", capacidade=" + capacidade +
+                ", motorista=" + motorista +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
