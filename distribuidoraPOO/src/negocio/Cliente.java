a315eefb43be5c0bfc852632e2c9e6b5ebf3d7fb
alvwaro <drawvaro@gmail.com>
2025-08-22 17:10:44 -0300

    nao aceita isso

--- distribuidoraPOO/src/negocio/Cliente.java
+++ distribuidoraPOO/src/negocio/Cliente.java
@@ -1,32 +1,35 @@
-private String tipo;
+package negocio;
 
-public Cliente(String nome, String cpf, String telefone, String endereco, String email, String tipo) {
-    super(nome, cpf, telefone, endereco, email);
-    this.tipo = tipo;
-}
+public class Cliente {
 
-public void realizarPedido(Pedido pedido) {
-    System.out.println("Cliente " + this.getNome() + " fez o pedido " + pedido.getId() + ".");
-    // Lógica para processar o pedido.
-}
+    public Cliente(String nome, String cpf, String telefone, String endereco, String email, String tipo) {
+        super(nome, cpf, telefone, endereco, email);
+        this.tipo = tipo;
+    }
 
-public void realizarPagamento(double valor, String metodo) {
-    System.out.println("Cliente " + this.getNome() + " pagou R$:" + valor + " usando:" + metodo);
-    // Lógica para processar o pagamento.
-}
+    public void realizarPedido(Pedido pedido) {
+        System.out.println("Cliente " + this.getNome() + " fez o pedido " + pedido.getId() + ".");
+        // Lógica para processar o pedido.
+    }
 
-public void status() {
-    System.out.println("Status do cliente:");
-    System.out.println("Nome: " + this.getNome());
-    System.out.println("CPF: " + this.getCpf());
-    System.out.println("Tipo: " + this.getTipo());
-}
+    public void realizarPagamento(double valor, String metodo) {
+        System.out.println("Cliente " + this.getNome() + " pagou R$:" + valor + " usando:" + metodo);
+        // Lógica para processar o pagamento.
+    }
 
-// gett e sett tipo
-public String getTipo() {
-    return tipo;
-}
+    public void status() {
+        System.out.println("Status do cliente:");
+        System.out.println("Nome: " + this.getNome());
+        System.out.println("CPF: " + this.getCpf());
+        System.out.println("Tipo: " + this.getTipo());
+    }
 
-public void setTipo(String tipo) {
-    this.tipo = tipo;
+    // gett e sett tipo
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
