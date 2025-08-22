2b3ca9d9e9a133a05e39072422220432b23ac32e
alvwaro <drawvaro@gmail.com>
2025-08-22 17:49:48 -0300

    AuxiliarAdm (pode aceitar)

--- distribuidoraPOO/src/negocio/AuxiliarAdm.java
+++ distribuidoraPOO/src/negocio/AuxiliarAdm.java
@@ -3,33 +3,32 @@ package negocio;
 public class AuxiliarAdm extends Funcionario {
     private String login;
 
-    public AuxiliarAdm(String nome, String cpf, String telefone, String endereco, String email, String cargo, double salario, String login) {
-        super(nome, cpf, telefone, endereco, email, cargo, salario);
+    public AuxiliarAdm(String cargo, double salario, String nome, int idade, String cpf, String telefone, String endereco, String email, String login) {
+        super(cargo, salario, nome, idade, cpf, telefone, endereco, email);
         this.login = login;
     }
 
     public void cadastrarFuncionario(Funcionario f) {
-        System.out.println("AuxiliarADM " + this.getNome() + " cadastrou o funcionário: " + f.getNome());
+        System.out.println("AuxiliarAdm " + this.getNome() + " cadastrou o funcionário: " + f.getNome());
     }
 
     public void cadastrarCaminhao(Caminhao c) {
-        System.out.println("AuxiliarADM " + this.getNome() + " cadastrou o caminhão da placa: " + c.getPlaca());
+        System.out.println("AuxiliarAdm " + this.getNome() + " cadastrou o caminhão da placa: " + c.getPlaca());
     }
 
     public void cadastrarCliente(Cliente c) {
-        System.out.println("AuxiliarADM " + this.getNome() + " cadastrou o cliente: " + c.getNome());
+        System.out.println("AuxiliarAdm " + this.getNome() + " cadastrou o cliente: " + c.getNome());
     }
 
     public void cadastrarProduto(Produto p) {
-        System.out.println("AuxiliarADM " + this.getNome() + " cadastrou o produto: " + p.getNome());
+        System.out.println("AuxiliarAdm " + this.getNome() + " cadastrou o produto: " + p.getNome());
     }
-
 
     public void permitirEntrada(Caminhao c) {
-        System.out.println("AuxiliarADM " + this.getNome() + " permitiu a entrada do caminhão da placa: " + c.getPlaca());
+        System.out.println("AuxiliarAdm " + this.getNome() + " permitiu a entrada do caminhão da placa: " + c.getPlaca());
     }
 
-    //gett e sett login
+    // Getters e Setters
     public String getLogin() {
         return login;
     }
