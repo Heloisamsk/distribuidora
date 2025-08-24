24743385254481c4c26f5a4756c29a910e19e540
arthur ramos <ramosarthuroliveira@gmail.com>
2025-08-24 20:49:24 -0300

    criando main para teste

--- distribuidoraPOO/src/ui/TelaPrincipal.java
+++ distribuidoraPOO/src/ui/TelaPrincipal.java
@@ -2,4 +2,8 @@ package ui;
 
 public class TelaPrincipal {
     // Essas classes chamam métodos da Fachada para executar ações, sem lógica de negócio.
+    public static void main(String[] args) {
+        System.out.println("=== Sistema da Distribuidora ===");
+
+    }
 }
