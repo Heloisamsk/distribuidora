bf59e3a0bb537c05c353da75d7ad4fe878bffefc
Heloisa Montebelo <134234754+Heloisamsk@users.noreply.github.com>
2025-08-25 14:37:03 -0300

    testando

--- distribuidoraPOO/src/ui/TelaPrincipal.java
+++ distribuidoraPOO/src/ui/TelaPrincipal.java
@@ -1,4 +1,5 @@
 package ui;
+import negocio.AuxiliarAdm;
 import negocio.Funcionario;
 import negocio.Motorista;
 import negocio.exceptions.PontoException;
@@ -10,17 +11,26 @@ public class TelaPrincipal {
         System.out.println("=== Sistema da Distribuidora ===");
         Motorista motorista = new Motorista();
         motorista.setMatricula("7856");
+
+        AuxiliarAdm auxiliar = new AuxiliarAdm();
+        auxiliar.setMatricula("0123");
       try {
           motorista.ponto(motorista.getMatricula());
           motorista.ponto(motorista.getMatricula());
 
           motorista.ponto(motorista.getMatricula());
-          motorista.baterEntrada(motorista.getMatricula());
+          motorista.baterEntrada(motorista.getMatricula()); // vai dar erro
+
       }catch (PontoException e) {
           System.out.println("Erro ao registrar ponto: " + e.getMessage());
       }
+      try {
+          auxiliar.baterEntrada(auxiliar.getMatricula());
+          auxiliar.baterEntrada(auxiliar.getMatricula()); // vai dar erro
+      } catch (PontoException e){
+          System.out.println("Erro ao registrar ponto: " + e.getMessage());
 
-
+      }
 
         System.out.println("continua rodando");
 
