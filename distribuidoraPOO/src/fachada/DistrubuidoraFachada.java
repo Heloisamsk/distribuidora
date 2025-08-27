906e7e9b2b205ea1cfb056698745a692c0db977b
riannavaz <rianna.vaz@ufape.edu.br>
2025-08-27 17:25:52 -0300

    metodos de cliente na fachada(comentado)

--- distribuidoraPOO/src/fachada/DistrubuidoraFachada.java
+++ distribuidoraPOO/src/fachada/DistrubuidoraFachada.java
@@ -1,11 +1,15 @@
 package fachada;
 import negocio.Cliente;
+import negocio.Estoque;
+import negocio.Pedido;
+import negocio.Produto;
+import negocio.exceptions.ClienteNaoExisteException;
+
+import java.util.ArrayList;
 
 public class DistrubuidoraFachada {
 /* Pacote : fachada
 Responsabilidade: Servir de intermediário entre UI e as camadas internas.
-
-
 
 DistribuidoraFachada
 
@@ -21,5 +25,20 @@ agendarEntradaCaminhao(Agendamento a)
 
 Chama Comunicação e Negócios.
 */
+    //metodos de cliente.java
+    public void realizarPedido(String cpfCliente, ArrayList<Produto> produtosDesejados, Estoque estoque) {
+        /*Cliente cliente = RepositorioCliente.buscarPorCpf(cpfCliente);
+        /* nao precisa desse if c a exception so se quiser uma segunda verificação
+        if (cliente == null || !cliente.isCadastrado()) {
+            throw new ClienteNaoExisteException("Cliente não cadastrado.");
+        }
+            cliente.realizarPedido(produtosDesejados, estoque);*/
+    }
+
+    public void realizarPagamento (String cpfCliente, Pedido pedido, double valorPago, Estoque estoque) {
+        /*Cliente cliente = RepositorioCliente.buscarPorCpf (cpfCliente);
+
+        cliente.realizarPagamento(pedido, valorPago, estoque);*/
+    }
 
 }
